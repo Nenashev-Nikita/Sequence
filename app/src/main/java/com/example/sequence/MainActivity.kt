a490e572e4017905578d49f3bc32a778ca0ff1db
Nenashev-Nikita <kacegar28@yandex.ru>
2024-10-16 20:04:31 +0700

    Initial commit

---
+++ app/src/main/java/com/example/sequence/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.sequence
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.sequence.ui.theme.SequenceTheme
+
+class MainActivity : ComponentActivity() {
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		enableEdgeToEdge()
+		setContent {
+			SequenceTheme {
+				Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+					Greeting(
+						name = "Android",
+						modifier = Modifier.padding(innerPadding)
+					)
+				}
+			}
+		}
+	}
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+	Text(
+		text = "Hello $name!",
+		modifier = modifier
+	)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+	SequenceTheme {
+		Greeting("Android")
+	}
+}
